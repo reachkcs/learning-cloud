a5a5a12df01e75ba4b0ec2092d0bfa6e588e0769
Sreedhar Chidambaram <schiambaram.ctr@Sreedhars-MacBook-Pro.local>
2025-06-06 09:54:01 -0400

    Enhancements

--- bin/pg_db_size.bash
+++ bin/pg_db_size.bash
@@ -6,9 +6,7 @@ run_pg_db_size_check() {
   cd /Users/schiambaram.ctr/KCS/scripts/learning-cloud/bin || return
 
   source "$SOURCE_FILE"
-  set -x
   python3 ./pg_db_size.py --host "${PGHOST}" --port "${PGPORT}" --name "${PGDATABASE}" --user "${PGUSER}" --password "${PGPASSWORD}" --clustername ${CLUSTER_NAME}
-  set +x
 }
 
 run_pg_db_size_check ~/KCS/prod/prod-ods ODS
